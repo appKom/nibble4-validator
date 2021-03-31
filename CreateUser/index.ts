d55734361ba01e33e10c46433f68a3a34c60649d
Martinskatvedt <martinskatvedt@gmail.com>
2021-03-31 17:21:32 +0200

    register user works

--- CreateUser/index.ts
+++ CreateUser/index.ts
@@ -1,16 +1,26 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import {authorizedPost, REGISTER_RFID_URI, fetchToken} from "../sharedCode/helperFunctions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+   
+    const token = await fetchToken();
+    
+    if (token) {
+        const response = await authorizedPost(token, {url:REGISTER_RFID_URI, body: req.body });
+        const json = await response.json();
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        context.res = {
+            status: response.status,
+            body: json
+        };
+    }
+    else {
+        context.res = {
+            status: 500,
+            body: {"error": "Could not fetch token"}
+        };
+    }
+    
 
 };
 
